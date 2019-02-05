b6b78fbeae2819b0f2f9587fc97fd85e8d57aa58
jkovacevic992 <jkovacevic992@gmail.com>
2019-02-05 11:02:33 +0100

    Optimized code, edited .phtml files

--- app/controller/IndexController.php
+++ app/controller/IndexController.php
@@ -20,10 +20,8 @@ class IndexController
             header('Location: ' . App::config('url'));
         } else {
             $connection = Db::connect();
-            date_default_timezone_set('Europe/Zagreb');
             $time = date("Y.m.d H:i:s");
-
-            $imageName = $_FILES["file"]["name"];
+            $imageName = str_replace(' ','_',$_FILES["file"]["name"]);
             $imageTmp = $_FILES["file"]["tmp_name"];
             move_uploaded_file($imageTmp, BP . "images/" . $imageName);
             $sql = 'insert into post (content, time, image) values (:content, :time, :image)';
